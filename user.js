b319e8fd201cb9e90ce9d9c180e9658de06e6357
saroj14-png <sarojani.powar@gmail.com>
2026-02-05 16:09:39 -0800

    renderPost fn

--- user.js
+++ user.js
@@ -1,5 +1,29 @@
-console.log();
-async function main() {
-  const id= localStorage.getItem("id");
-  const posts = await fetch("https://jsonplaceholder.typicode.com/users");
-  userListEl.innerHTML=posts.json();
+const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
+
+async function onSearchChange(event) {
+   const id = event.target.value;
+   renderPosts(id);
+ }
+async function renderPosts(userId) {
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId ||id}`
+  );
+  const postsData = await posts.json();
+  postListEl.innerHTML = postsData
+    .map(
+      post => PostHTML(post)).join('');
+ }
+
+ function PostHTML(post) {
+  return `
+    <div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>`;
+ }
+renderPosts(id);
